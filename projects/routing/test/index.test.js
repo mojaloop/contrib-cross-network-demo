f7cd086f20a8560704482abf95e0f7460371713b
Matt de Haast <matt.dehaast@coil.com>
2019-03-11 13:37:45 +0200

    testing: add basic test

--- projects/routing/test/index.test.js
+++ projects/routing/test/index.test.js
@@ -1,7 +1,11 @@
 const assert = require('assert')
 
 describe('index', function () {
-  it('resolves true', function () {
-    assert.strictEqual(true, true)
+  it('creates an instance of an initialized server', async function () {
+    const server = await require('../src/index')
+
+    assert.notStrictEqual(server.info.started, 0)
+
+    server.stop()
   })
 })
